9129797fb920dff489cc4e50846843be98d0291c
Haziq Abiyyu Mahdy <haziqabiyyu@gmail.com>
2025-06-09 23:17:48 +0700

    add cors configuration

--- backend/src/main/java/com/std_data_mgmt/app/configs/WebConfiguration.java
+++ backend/src/main/java/com/std_data_mgmt/app/configs/WebConfiguration.java
@@ -1,24 +1,37 @@
 package com.std_data_mgmt.app.configs;
 
+import com.std_data_mgmt.app.security.rbac.RoleAuthorizationInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import com.std_data_mgmt.app.security.rbac.RoleAuthorizationInterceptor;
 
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
-    
+    @Value("${cors.allowed-origins}")
+    private String[] allowedOrigins;
+
     @Autowired
     private RoleAuthorizationInterceptor roleAuthorizationInterceptor;
-    
+
     @Override
     public void addInterceptors(@NonNull InterceptorRegistry registry) {
         registry.addInterceptor(roleAuthorizationInterceptor)
                 .addPathPatterns("/api/**")
                 .excludePathPatterns("/api/v1/auth/**");
+    }
+
+    @Override
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
